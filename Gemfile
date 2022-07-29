aca137670a285fe4fa4711a08c70b8e2d64cccc5
Gimmy <gheorghe.popescu@aserto.com>
2022-07-29 13:53:50 +0300

    validate jwt token

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem "http"
 
 gem "aserto-rails"
 
+gem "jwt"
+
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 gem "rack-cors"
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  before_action :validate_jwt
+
   rescue_from Aserto::AccessDenied do |exception|
     render json: { error: exception.message }, status: :unauthorized
   end
@@ -9,5 +11,11 @@ class ApplicationController < ActionController::API
     Rails.logger.error exception.backtrace.inspect if Rails.env.development?
 
     render json: { error: exception.message }, status: :internal_server_error
+  end
+
+  private
+
+  def validate_jwt
+    Auth::VerifyJwt.call(request.headers["Authorization"].split.last)
   end
 end

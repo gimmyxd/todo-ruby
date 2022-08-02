d86ccc7bb4149767a7a00b78246dfd9bb145fa95
Gimmy <gheorghe.popescu@aserto.com>
2022-08-02 10:30:57 +0300

    fix error precedence

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,14 @@
 class ApplicationController < ActionController::API
   before_action :validate_jwt
 
-  rescue_from Aserto::AccessDenied do |exception|
-    render json: { error: exception.message }, status: :unauthorized
-  end
-
   rescue_from StandardError do |exception|
     Rails.logger.error exception.backtrace.inspect if Rails.env.development?
 
     render json: { error: exception.message }, status: :internal_server_error
+  end
+
+  rescue_from Aserto::AccessDenied do |exception|
+    render json: { error: exception.message }, status: :forbidden
   end
 
   private
